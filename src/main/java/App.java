fcac8ecb6066dba927dc800048ee25275b2ee034
Aimee <aelizabethwhitaker@yahoo.com>
2020-10-13 10:12:55 -0500

    merge conflict?

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world.");
+        System.out.println("Hello globe.");
         System.out.println("Aimee was here");
     }
 }
