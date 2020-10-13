0e0dc7dddbc0194fd8e46f5215e700d0a52a91e3
LoganLang <langloganr@gmail.com>
2020-10-13 10:19:39 -0500

    Will this make a merge conflict?

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world. Aimee better not change anything here.");
-        System.out.println("Aimee was here");
+        System.out.println("Aimee was here.");
+
+        //Link to this comment.
     }
 }
