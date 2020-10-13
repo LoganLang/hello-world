b4f90538183a71279e1decbe4a992c2742c4ff3d
Aimee <aelizabethwhitaker@yahoo.com>
2020-10-13 10:17:29 -0500

    facts

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world. Aimee better not change anything here.");
+        System.out.println("Hello world (the earth is not flat). Aimee better not change anything here.");
         System.out.println("Aimee was here");
     }
 }
